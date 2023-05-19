82a2ab311f64c86f59a008a474a53abd33bf5263
MORGANJASON <420morganjason@gmail.com>
2023-05-19 13:28:43 +0300

    completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
 
-def greet(name):
-    pass
+def greet_programmer():
+    print("Hello, programmer!")
+
+
+def greet(name="Naureen"):
+    print(f"Hello, {name}!")
+
+
+greet_programmer()
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+
+greet_with_default()
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+
+Add = add(45, 55)
+
 
 def halve(number):
-    pass
+    return (int(number)) / 2
+
+def halve(number):
+    return (float(number)) / 2
+
